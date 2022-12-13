6582ae7427e728eb5cd2694b06ba27df6bcf79d0
Akshat <akshat.pande@outlook.com>
2022-12-13 20:43:12 +0000

    Modification Details

--- Main.py
+++ Main.py
@@ -2,5 +2,4 @@ import streamlit as st
 
 st.write("Individual Project")
 st.title("Carbon Blockchain: Footprint, Credit, and Index")
-st.subheader("Author - Akshat Pande ")
-st.subheader("Supervisor - Theodoros Constantinides ")
+st.caption("Author - Akshat Pande \n Supervisor - Theodoros Constantinides")
