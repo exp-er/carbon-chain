c1d601014d634f6f4b3242bf5e8b0e6a0563f68d
Akshat <akshat.pande@outlook.com>
2022-12-14 11:20:52 +0000

    Module Redundancy Removed

--- CC.py
+++ CC.py
@@ -3,7 +3,6 @@
 import streamlit as st 
 import streamlit.components.v1 as components
 import requests
-from bs4 import BeautifulSoup
 
 x='<a class="minerstat-widget" title="BTC mining calculator" data-coin="BTC" data-algo="SHA-256" data-info="yes" data-style="light" data-color="" data-unit="TH" data-hashrate="100" data-width="300" rel="nofollow" href="https://minerstat.com/coin/BTC">Bitcoin mining calculator</a><script async src="https://api.minerstat.com/v2/widgets/coin.js" charset="utf-8"></script>'
 url = "https://api.minerstat.com/v2/coins?list=BTC"
